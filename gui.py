c4211dee87e851abeb33a0166d961078637f8bae
Arvi <artemtolstykn@gmail.com>
2026-06-22 14:34:31 +0300

    connect painting logic to gui

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import tkinter as tk
-from tkinter import filedialog
-from core import area_selector
+from tkinter import filedialog, messagebox
+from core import area_selector, image_loader, painter
 import config
 
 def run():
@@ -9,8 +9,24 @@ def run():
     def choose_file():
         file_path_label.config(text=filedialog.askopenfilename(filetypes=[("Images", "*.jpg;*.jpeg;*.png")]))
     def select_area():
+        nonlocal upper_left, lower_right
         upper_left, lower_right = area_selector.select_area()
         area_label.config(text=f"upper left: x={upper_left[0]}, y={upper_left[1]}\nlower right: x={lower_right[0]}, y={lower_right[1]}")
+    def paint():
+        if not messagebox.askyesno("confirm", "start painting?\nESC - break"):
+            return
+        if file_path_label.cget('text') == "":
+            messagebox.showwarning("Warning", "file field is empty")
+            return
+        elif delay.get() == "":
+            messagebox.showwarning("Warning", "delay field is empty")
+            return
+        elif area_label.cget('text') == "":
+            messagebox.showwarning("Warning", "area field is empty")
+            return
+        else:
+            img = image_loader.load_image(file_path_label.cget('text'), invert_var.get(), config.THRESHOLD)
+            painter.paint(img, float(delay.get()), upper_left, lower_right)
     main = tk.Tk()
     main.title("PixelPainter")
     main.geometry("400x300")
@@ -39,7 +55,7 @@ def run():
     select_area_button = tk.Button(main, text="Select area", command=select_area)
     select_area_button.pack(pady=5)
 
-    start_button = tk.Button(main, text="Start")
+    start_button = tk.Button(main, text="Start", command=paint)
     start_button.pack(pady=10)
     main.mainloop()
 
